d6fc84137b3824bcce832ebc31a57dbef1535a80
Sergei Grin <grinzzly@gmail.com>
2017-06-24 12:27:13 +0300

    MongoDB

--- app/models/Music.js
+++ app/models/Music.js
@@ -1,30 +1,14 @@
 ;(()=>{
-/*var mongoose = require('mongoose');
-var Schema = mongoose.Schema;
+    var mongoose = require('mongoose');
+    var Schema = mongoose.Schema;
 
-var musicSchema = new Schema({
-	singer : {type: String},
-	song : {type: String},
-	genre : {type: String},
-	year : {type: String}
-});
-
-module.exports = mongoose.model('Music', musicSchema);*/
-
-    var orm = require("orm");
-
-    orm.connect("mysql://Grinzzly:EZxCqBQ81a!EZxCqBQ81a!@klikachalange.csj2th7yy5el.us-east-2.rds.amazonaws.com/KlikaChalange", function (err, db) {
-        if (err){
-            throw err;
-        }
-
-        // var musicSchema = new Schema({
-        //     singer : {type: String},
-        //     song : {type: String},
-        //     genre : {type: String},
-        //     year : {type: String}
-        // });
-
-        console.log(db);
+    var musicSchema = new Schema({
+	    singer : {type: String},
+	    song : {type: String},
+	    genre : {type: String},
+	    year : {type: String}
     });
+
+    module.exports = mongoose.model('Music', musicSchema);
+
 })();
--- server.js
+++ server.js
@@ -4,10 +4,10 @@ var logger = require('morgan');
 var bodyParser = require('body-parser');
 var favicon = require('serve-favicon');
 var app      = express();
-// var mongoose = require('mongoose');
+var mongoose = require('mongoose');
 var port  	 = process.env.PORT || 8888;
-//EZxCqBQ81a!EZxCqBQ81a!
-// mongoose.connect('mongodb://usuario2:password@ec2-52-56-133-40.eu-west-2.compute.amazonaws.com:27017/admin');
+
+mongoose.connect('mongodb://usuario2:password@ec2-52-56-133-40.eu-west-2.compute.amazonaws.com:27017/admin');
 
 app.set('view engine', 'ejs');
 app.use(logger('dev'));
